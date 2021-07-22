32a8e513381826254c07238af10b6df0d2e9e314
chihab_eddine98 <ga_benamara@esi.dz>
2021-07-22 22:54:42 +0400

    Get argmax and argmin from v_map (useful for best_action function on UBFMS)

--- src/engine.cpp
+++ src/engine.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <iostream>
+#include <map>
 
 #include "bitboard.h"
 #include "endgame.h"
@@ -27,7 +28,7 @@ using namespace Stockfish;
 Value negamax(Position& pos, Depth depth, int alpha, int beta, Color color)
 {
     //depth end reached ? or we actually hit a win/lose condition?
-    if (depth == 0 || pos.is_draw(pos.game_ply()))
+    if (depth == 0)
     {
         return  color ? Eval::evaluate(pos) : -Eval::evaluate(pos);
     }
@@ -93,6 +94,52 @@ Move search(Position& pos,Depth depth)
     return best_move(pos,depth);
 }
 
+
+
+// In this section we will code the UBFMS algorithm
+// using NNUE evaluation function by stockfish 14
+
+
+// This function is a utils function which wil help us
+// to get min or max of a hashmap structure given on parameter
+
+Move get_arg_best_map(std::map<Move,Value> v_map , bool max_value)
+{
+    std::map<Move,Value>::iterator best;
+
+    if (max_value)
+    {
+        best = std::max_element(v_map.begin(),v_map.end(),[] (const std::pair<Move,Value>& a, const std::pair<Move,Value>& b)->bool{ return a.second < b.second; } );
+        std::cout << " Max  : "  << best->first << " , " << " Min :" << best->second << "\n";
+    }
+    else
+    {
+        best = std::min_element(v_map.begin(),v_map.end(),[] (const std::pair<Move,Value>& a, const std::pair<Move,Value>& b)->bool{ return a.second < b.second; } );
+        std::cout << " Min  : "  << best->first << " , " << " Max :" << best->second << "\n";
+
+    }
+
+    return best->first;
+}
+
+
+// v_map : is the value function which takes an action as key
+//         and NNUE value as value
+std::map<Move, Value > v_map;
+
+Move best_action(Position& pos)
+{
+    if (pos.side_to_move() == WHITE)
+    {
+
+    }
+    else
+    {
+
+    }
+
+}
+
 void engine_init(int argc, char* argv[])
 {
     std::cout << engine_info() << std::endl;
@@ -156,9 +203,44 @@ int main(int argc, char* argv[])
 {
 
     engine_init(argc,argv);
-    debug();
+    std::map<Move ,Value> x;
+
+    //debug();
+
+    std::string StartFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    Position pos;
+    StateInfo st;
+
+    StateListPtr states(new std::deque<StateInfo>(1));
+
+    pos.set(StartFEN, false,&states->back(),Threads.main());
+
+    MoveList<LEGAL> moves = MoveList<LEGAL>(pos);
+    std::cout << "Evaluation : " <<Eval::evaluate(pos) << std::endl;
+    std::cout << "----------------- \n" << std::endl;
+    std::cout << pos << std::endl;
 
 
+    for (auto& m : moves) {
+        sync_cout << UCI::move(m,false) << sync_endl;
+        pos.do_move(m,st);
+        x[m] = Eval::evaluate(pos);
+        std::cout << "Evaluation : " <<Eval::evaluate(pos) << std::endl;
+        std::cout << pos << std::endl;
+        pos.undo_move(m);
+
+    }
+
+    for (std::map<Move, Value>::iterator p = x.begin();
+         p != x.end(); ++p ) {
+        sync_cout << " Move : " << UCI::move(p->first,false)
+             << ", Value : " << p->second << sync_endl;
+    }
+
+    sync_cout << sync_endl;
+
+    Move b = get_arg_best_map(x,true);
+    sync_cout << " Best arg : " << UCI::move(b,false) << sync_endl;
     return 0;
 }
 
